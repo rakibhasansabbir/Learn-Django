00c37556d0697ecc79a2be85354d5a7dac875812
rakibhasansabbir <rakibhasansabbir@yahoo.com>
2019-11-28 03:06:01 +0600

    add style template, create model and migration

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
+from datetime import datetime
+
 from django.db import models
 
+
 # Create your models here.
+
+class Posts(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField(blank=False, null=False)
+    created_by = models.CharField(max_length=80)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    # return HttpResponse('Hello from view')
+    return render(request, 'index.html', {'title': 'latest post'})
